a5143905dd9c6cccd927bdfe04c0bb6e31cc5d5f
ebrahim afridi <afridiebr@student.iul.ac.in>
2024-09-03 00:21:34 +0530

    learning hono

--- hono-app/src/index.ts
+++ hono-app/src/index.ts
@@ -1,9 +1,31 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+// Auth Function
+async function authMiddleware(c: any, next: any) {
+  if (c.req.header === "Authorization") {
+    // Do validation
+    await next();
+  } else {
+    return c.text("You donot have access. ");
+  }
+}
 
-export default app
+// Middleware
+// app.use(authMiddleware);
+
+// Routes
+app.post("/", authMiddleware, async (c) => {
+  const body = await c.req.json();
+  console.log(body);
+  console.log(c.req.header("Authorized"));
+  console.log(c.req.query("params"));
+
+  return c.text("Hello Hono!");
+});
+app.post("/user", (c) => {
+  return c.text("Hello Hono!");
+});
+
+export default app;
