677334b078f97b2b527acb195403f5a494403060
ebrahim afridi <afridiebr@student.iul.ac.in>
2024-09-04 00:40:27 +0530

    add end-point for protected routing

--- medium/backend/src/index.ts
+++ medium/backend/src/index.ts
@@ -1,7 +1,7 @@
 import {Hono} from 'hono'
 import {PrismaClient} from '@prisma/client/edge'
 import {withAccelerate} from '@prisma/extension-accelerate'
-import {sign} from "hono/jwt"
+import {sign, verify} from "hono/jwt"
 
 const app = new Hono<{
     Bindings: {
@@ -9,6 +9,22 @@ const app = new Hono<{
         JWT_SECRET_KEY: string,
     }
 }>()
+
+// Middleware
+
+app.use("/api/v1/blog/*", async (c, next) => {
+    const header = c.req.header("authorization") || "";
+    const token = header.split(" ")[1]
+
+    // Response will have an id because we signed the jwt with an id while signing in.
+    const response = await verify(token, c.env.JWT_SECRET_KEY);
+    if (response.id) {
+        await next()
+    } else {
+        c.status(403)
+        return c.json({error: "unauthorized"})
+    }
+})
 
 app.post("/api/v1/signup", async (c) => {
     const prisma = new PrismaClient({
