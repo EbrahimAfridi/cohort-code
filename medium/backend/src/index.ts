caa8d540d2a14ea912c07030db1e8a54f6ec9c1c
ebrahim afridi <afridiebr@student.iul.ac.in>
2024-09-03 22:15:11 +0530

    setup prisma with hono

--- medium/backend/src/index.ts
+++ medium/backend/src/index.ts
@@ -1,9 +1,42 @@
-import { Hono } from 'hono'
+import {Hono} from 'hono'
+import {PrismaClient} from '@prisma/client/edge'
+import {withAccelerate} from '@prisma/extension-accelerate'
 
-const app = new Hono()
+
+const app = new Hono<{
+    Bindings: {
+        DATABASE_URL: string
+    }
+}>()
+
+app.post("/api/v1/signup", async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+
+    const body = 
+
+    return c.text('Hello Hono!')
+})
+
+app.post("/api/v1/signin", (c) => {
+    return c.text('Hello Hono!')
+})
+
+app.post("/api/v1/blog", (c) => {
+    return c.text('Hello Hono!')
+})
+
+app.put("/api/v1/blog", (c) => {
+    return c.text('Hello Hono!')
+})
+
+app.get("/api/v1/blog/:id", (c) => {
+    return c.text('Hello Hono!')
+})
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+    return c.text('Hello Hono!')
 })
 
 export default app
