10bcf0d974bc44fec326728aacb4f5566877ebf4
ebrahim afridi <afridiebr@student.iul.ac.in>
2024-09-03 23:54:11 +0530

    add sign-in end-point

--- medium/backend/src/index.ts
+++ medium/backend/src/index.ts
@@ -1,11 +1,12 @@
 import {Hono} from 'hono'
 import {PrismaClient} from '@prisma/client/edge'
 import {withAccelerate} from '@prisma/extension-accelerate'
-
+import {sign} from "hono/jwt"
 
 const app = new Hono<{
     Bindings: {
-        DATABASE_URL: string
+        DATABASE_URL: string,
+        JWT_SECRET_KEY: string,
     }
 }>()
 
@@ -16,18 +17,34 @@ app.post("/api/v1/signup", async (c) => {
 
     const body = await c.req.json();
 
-    await prisma.user.create({
+    const user = await prisma.user.create({
         data: {
             email: body.email,
             password: body.password
         },
     })
 
-    return c.text('Hello Hono!')
+    const token = await sign({id: user.id}, c.env.JWT_SECRET_KEY)
+
+    return c.json({jwt: token})
 })
 
-app.post("/api/v1/signin", (c) => {
-    return c.text('Hello Hono!')
+app.post("/api/v1/signin", async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+
+    const body = await c.req.json();
+
+    const user = await prisma.user.findUniqueOrThrow({
+        where: {
+            email: body.email,
+        },
+    })
+
+    const jwt = await sign({id: user.id}, c.env.JWT_SECRET_KEY)
+
+    return c.json({jwt})
 })
 
 app.post("/api/v1/blog", (c) => {
