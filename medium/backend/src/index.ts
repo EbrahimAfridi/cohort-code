6c08c205ac0e1666f04c43c251aae017757d7bc3
ebrahim afridi <afridiebr@student.iul.ac.in>
2024-09-06 21:43:39 +0530

    refactor index.ts

--- medium/backend/src/index.ts
+++ medium/backend/src/index.ts
@@ -1,82 +1,29 @@
-import {Hono} from 'hono'
-import {PrismaClient} from '@prisma/client/edge'
-import {withAccelerate} from '@prisma/extension-accelerate'
-import {sign, verify} from "hono/jwt"
+import { Hono } from "hono";
 
-const app = new Hono<{
-    Bindings: {
-        DATABASE_URL: string,
-        JWT_SECRET_KEY: string,
-    }
-}>()
+const app = new Hono();
 
-// Middleware
-
-app.use("/api/v1/blog/*", async (c, next) => {
-    const header = c.req.header("authorization") || "";
-    const token = header.split(" ")[1]
-
-    // Response will have an id because we signed the jwt with an id while signing in.
-    const response = await verify(token, c.env.JWT_SECRET_KEY);
-    if (response.id) {
-        await next()
-    } else {
-        c.status(403)
-        return c.json({error: "unauthorized"})
-    }
+app.post("/api/v1/user/signup", (c) => {
+    return c.text("Hello from Hono.")
 })
 
-app.post("/api/v1/signup", async (c) => {
-    const prisma = new PrismaClient({
-        datasourceUrl: c.env.DATABASE_URL,
-    }).$extends(withAccelerate())
-
-    const body = await c.req.json();
-
-    const user = await prisma.user.create({
-        data: {
-            email: body.email,
-            password: body.password
-        },
-    })
-
-    const token = await sign({id: user.id}, c.env.JWT_SECRET_KEY)
-
-    return c.json({jwt: token})
-})
-
-app.post("/api/v1/signin", async (c) => {
-    const prisma = new PrismaClient({
-        datasourceUrl: c.env.DATABASE_URL,
-    }).$extends(withAccelerate())
-
-    const body = await c.req.json();
-
-    const user = await prisma.user.findUniqueOrThrow({
-        where: {
-            email: body.email,
-        },
-    })
-
-    const jwt = await sign({id: user.id}, c.env.JWT_SECRET_KEY)
-
-    return c.json({jwt})
+app.post("/api/v1/user/signin", (c) => {
+    return c.text("Hello from Hono.")
 })
 
 app.post("/api/v1/blog", (c) => {
-    return c.text('Hello Hono!')
+    return c.text("Hello from Hono.")
 })
 
 app.put("/api/v1/blog", (c) => {
-    return c.text('Hello Hono!')
+    return c.text("Hello from Hono.")
 })
 
-app.get("/api/v1/blog/:id", (c) => {
-    return c.text('Hello Hono!')
+app.get("/api/v1/blog", (c) => {
+    return c.text("Hello from Hono.")
 })
 
-app.get('/', (c) => {
-    return c.text('Hello Hono!')
+app.get("/api/v1/blog/blog", (c) => {
+    return c.text("Hello from Hono.")
 })
 
 export default app
