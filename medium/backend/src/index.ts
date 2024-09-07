f94f94bc1ff69f3d18de1aa0ce28281d9060c7a1
ebrahim afridi <afridiebr@student.iul.ac.in>
2024-09-07 10:45:56 +0530

    add sign up and sign in end points

--- medium/backend/src/index.ts
+++ medium/backend/src/index.ts
@@ -1,29 +1,85 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET_KEY: string;
+  };
+}>();
 
-app.post("/api/v1/user/signup", (c) => {
-    return c.text("Hello from Hono.")
-})
+app.post("/api/v1/user/signup", async (c) => {
+  const body = await c.req.json();
 
-app.post("/api/v1/user/signin", (c) => {
-    return c.text("Hello from Hono.")
-})
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name: body.name,
+        password: body.password,
+        email: body.email,
+      },
+    });
+    const jwt = await sign(
+      {
+        id: user.id,
+      },
+      c.env.JWT_SECRET_KEY
+    );
+    return c.text(jwt, 200);
+  } catch (error) {
+    console.error(error);
+    return c.text("Invalid", 411);
+  }
+});
+
+app.post("/api/v1/user/signin", async (c) => {
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        password: body.password,
+        email: body.email,
+      },
+    });
+    if (!user) {
+      return c.text("Incorrect credentials.", 403);
+    }
+    const jwt = await sign(
+      {
+        id: user.id,
+      },
+      c.env.JWT_SECRET_KEY
+    );
+    return c.text(jwt, 200);
+  } catch (error) {
+    console.error(error);
+    return c.text("Invalid", 411);
+  }
+});
 
 app.post("/api/v1/blog", (c) => {
-    return c.text("Hello from Hono.")
-})
+  return c.text("Hello from Hono.");
+});
 
 app.put("/api/v1/blog", (c) => {
-    return c.text("Hello from Hono.")
-})
+  return c.text("Hello from Hono.");
+});
 
 app.get("/api/v1/blog", (c) => {
-    return c.text("Hello from Hono.")
-})
+  return c.text("Hello from Hono.");
+});
 
 app.get("/api/v1/blog/blog", (c) => {
-    return c.text("Hello from Hono.")
-})
+  return c.text("Hello from Hono.");
+});
 
-export default app
+export default app;
