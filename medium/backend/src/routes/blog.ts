40f0f4a6a766626b6b18eae92831c3adc5065b3e
ebrahim afridi <afridiebr@student.iul.ac.in>
2024-09-07 11:28:03 +0530

    add get bulk blogs end point

--- medium/backend/src/routes/blog.ts
+++ medium/backend/src/routes/blog.ts
@@ -66,10 +66,21 @@ blogRouter.get("/", async (c) => {
     });
     return c.json(blog);
   } catch (error) {
-    return c.json({message: "Error while fetching blog"}, 400)
+    return c.json({ message: "Error while fetching blog" }, 400);
   }
 });
 
-blogRouter.get("/bulk", (c) => {
-  return c.text("Hello from Hono.");
+// TODO: ADD PAGINATION LATER
+blogRouter.get("/bulk", async (c) => {
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const blogs = await prisma.blog.findMany();
+    return c.json(blogs);
+  } catch (error) {
+    return c.json({ message: "Error while fetching blogs" }, 400);
+  }
 });
