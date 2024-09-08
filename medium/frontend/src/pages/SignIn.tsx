f2d06ec8623734dd417ca2f3e78524ffb95fd760
ebrahim afridi <afridiebr@student.iul.ac.in>
2024-09-08 10:34:18 +0530

    add responsiveness to signin and signup page

--- medium/frontend/src/pages/SignIn.tsx
+++ medium/frontend/src/pages/SignIn.tsx
@@ -3,11 +3,11 @@ import Quote from "../components/Quote.tsx";
 
 export default function SignInPage() {
     return (
-        <div className="grid grid-cols-2">
+        <div className="grid grid-cols-1 lg:grid-cols-2">
             <div>
                 <Auth type={"signin"}/>
             </div>
-            <div className="none lg:block">
+            <div className="hidden lg:block">
                 <Quote/>
             </div>
         </div>
