23b6736e60b74bd0aaa82075b2b58dc05f1dc88d
ebrahim afridi <afridiebr@student.iul.ac.in>
2024-09-11 12:32:31 +0530

    schema updated

--- nextjs-backend/app/api/user/route.ts
+++ nextjs-backend/app/api/user/route.ts
@@ -1,13 +1,26 @@
 import {NextRequest} from "next/server";
+import {PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient(); // Prisma instance
 
 export async function POST(req: NextRequest) {
     // extract the body from request
     const body = await req.json();
+    try {
+        // store in database
+        const user = await prisma.user.create({
+            data: {
+                username: body.username,
+                password: body.password,
+            }
+        });
 
-    // store in database
-    console.log(body);
-
-    return Response.json({
-        message: "You are logged in",
-    });
+        return Response.json({
+            message: "You are signed in",
+            id: user.id
+        });
+    } catch (error) {
+        console.error(error);
+        throw new Error("Something went wrong");
+    }
 }
--- nextjs-backend/app/page.tsx
+++ nextjs-backend/app/page.tsx
@@ -1,24 +1,5 @@
-import axios from "axios";
-
-async function getUserDetails() {
-    const response = await axios.get("http://localhost:3000/api/user");
-    console.log(response);
-    return response.data;
-}
-
 export default async function Home() {
-    const userDetails = await getUserDetails();
-
-    return (
-        <div className={"text-center flex flex-col justify-center h-screen"}>
-            <div className="flex justify-center">
-                <div className="border p-8 rounded">
-                    <div>
-                        Hi there, {userDetails.name}
-                    </div>
-                    {userDetails.email}
-                </div>
-            </div>
-        </div>
-    );
+    return <div className={"text-center flex flex-col justify-center h-screen"}>
+        Hi there,
+    </div>
 }
