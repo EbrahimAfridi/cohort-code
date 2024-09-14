021b7a51ac51ed8934e673fec8cf1944a5fa799a
ebrahim afridi <afridiebr@student.iul.ac.in>
2024-09-14 14:01:26 +0530

    add appbar and next-auth

--- medium/frontend/src/components/OptionsModal.tsx
+++ medium/frontend/src/components/OptionsModal.tsx
@@ -4,9 +4,10 @@ function OptionsModal({id}: {id: string}) {
     return (
         <div className={"bg-zinc-100 shadow rounded "}>
             <button className={"w-full p-2 text-xs font-medium text-black hover:bg-blue-800 hover:text-white rounded"}>
-                <Link to={`/blog/edit/${id}`}>
-                    Edit
-                </Link>
+                {/*<Link to={`/blog/edit/${id}`}>*/}
+                {/*    Edit*/}
+                {/*</Link>*/}
+                <a href={`http://localhost:5173/blog/edit/${id}`}>Edit</a>
             </button>
             <button className={"w-full p-2 text-xs font-medium text-black hover:bg-red-800 hover:text-white rounded"}>
                 <Link to={"/delete"}>
